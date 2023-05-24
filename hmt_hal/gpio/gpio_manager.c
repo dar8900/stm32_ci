6400d403da739d1b71e3031c94b16dfbfff4939e
Dario Sadel VB <dar8900@gmail.com>
2023-05-24 17:23:07 +0200

    Iniziato primo inserimento per le strutture dei gpio

--- hmt_hal/gpio/gpio_manager.c
+++ hmt_hal/gpio/gpio_manager.c
@@ -1,14 +1,27 @@
-#include <stdint.h>
-#if defined STM32F1
-    #include <stm32f1xx_hal.h>
-#endif
-#if defined STM32F4
-    #include <stm32f4xx_hal.h>
-#endif
-
 #include "gpio_manager.h"
 
-void TogglePin(GPIO_TypeDef *Port, uint16_t Pin)
+static gpio_value ValueToDigital(uint32_t Val)
 {
-	HAL_GPIO_TogglePin(Port, Pin);
+    gpio_value NewVal = LOW;
+    if(Val == 0){
+        NewVal = LOW;
+     } else {
+        NewVal = HIGH;
+     }
+    return NewVal;
+}
+
+
+bool hmt_togglePin(gpio_def *Gpio)
+{
+    bool Toggled = false;
+    uint32_t odr = READ_REG(Gpio->port->ODR);
+    Gpio->oldVal = ValueToDigital(odr);
+    LL_GPIO_TogglePin(Gpio->port, Gpio->pinDef.Pin);
+    if(READ_REG(Gpio->port->ODR != odr)){
+        odr = READ_REG(Gpio->port->ODR);
+        Toggled = true;
+        Gpio->actualVal = ValueToDigital(odr);
+    }
+    return Toggled;
 }
--- hmt_hal/gpio/gpio_manager.h
+++ hmt_hal/gpio/gpio_manager.h
@@ -1,7 +1,70 @@
 #ifndef GPIO_MANAGER_H
 #define GPIO_MANAGER_H
 
-void TogglePin(GPIO_TypeDef *Port, uint16_t Pin);
+#if defined STM32F1
+    #include <stm32f1xx_hal.h>
+    #include <stm32f1xx_ll_gpio.h>
+#endif
+#if defined STM32F4
+    #include <stm32f4xx_hal.h>
+#endif
+#include <stdint.h>
+#include <stdbool.h>
 
+typedef enum gpio_value
+{
+    LOW = GPIO_PIN_RESET,
+    HIGH = GPIO_PIN_SET
+}gpio_value;
+// typedef enum
+// {
+//     PORT_A = GPIOA,
+//     PORT_B = GPIOB,
+//     PORT_C = GPIOC,
+//     PORT_D = GPIOD,
+//     PORT_E = GPIOE,
+// #ifdef GPIOF
+//     PORT_F = GPIOF,
+// #endif
+// #ifdef GPIOG
+//     PORT_G = GPIOG,
+// #endif    
+// #ifdef GPIOH
+//     PORT_H = GPIOH    
+// #endif
+// }gpio_port;
+
+typedef enum gpio_pin
+{
+    IO_PIN_0  =  LL_GPIO_PIN_0,
+    IO_PIN1   =  LL_GPIO_PIN_1,
+    IO_PIN2   =  LL_GPIO_PIN_2,
+    IO_PIN3   =  LL_GPIO_PIN_3,
+    IO_PIN4   =  LL_GPIO_PIN_4,
+    IO_PIN5   =  LL_GPIO_PIN_5,
+    IO_PIN6   =  LL_GPIO_PIN_6,
+    IO_PIN7   =  LL_GPIO_PIN_7,
+    IO_PIN8   =  LL_GPIO_PIN_8,
+    IO_PIN9   =  LL_GPIO_PIN_9,
+    IO_PIN10  =  LL_GPIO_PIN_10,
+    IO_PIN11  =  LL_GPIO_PIN_11,
+    IO_PIN12  =  LL_GPIO_PIN_12,
+    IO_PIN13  =  LL_GPIO_PIN_13,
+    IO_PIN14  =  LL_GPIO_PIN_14,
+    IO_PIN15  =  LL_GPIO_PIN_15,
+    IO_MAX_PIN = 17
+}gpio_pin;
+
+typedef struct 
+{
+    GPIO_TypeDef            *port;
+    LL_GPIO_InitTypeDef      pinDef;
+    gpio_value              actualVal;
+    gpio_value              oldVal;
+}gpio_def;
+
+
+
+bool hmt_togglePin(gpio_def *Gpio);
 
 #endif
