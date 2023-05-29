35c014bdc0af57258a176f200bbc083b8830bdb0
Dario Sadel VB <dar8900@gmail.com>
2023-05-29 13:29:23 +0200

    Pulito main

--- src/main.c
+++ src/main.c
@@ -1,26 +1,8 @@
 #include "main.h"
 
 
-void initGPIO()
-{
-    GPIO_InitTypeDef GPIO_Config;
-
-    GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_Config.Pull = GPIO_NOPULL;
-    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
-
-    GPIO_Config.Pin = LED_PIN;
-
-    LED_PORT_CLK_ENABLE();
-    HAL_GPIO_Init(LED_PORT, &GPIO_Config);
-}
-
 int main(void)
 {
-    HAL_Init();
-    initGPIO();
-    // 1kHz ticks
-    HAL_SYSTICK_Config(SystemCoreClock / 1000);
 
     for (;;)
         __WFI();
