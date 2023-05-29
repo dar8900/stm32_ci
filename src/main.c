e5d23c70679d91a09bdad833cf7f3729196fda66
Dario Sadel VB <dar8900@gmail.com>
2023-05-29 17:10:25 +0200

    Aggiunto blink di esempio con la libreria nuova

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 
 int main(void)
 {
-
+    gpio_def LedPin = {.port = GPIOA, .pin = IO_PIN_5, .mode = OUTPUT, .pull = PUSH_PULL};
+    hmt_RccInit();
+    hmt_GpioInit(&LedPin);
     for (;;)
-        __WFI();
+    {
+        hmt_GpioTogglePin(&LedPin);
+        hmt_Delay(1000);
+    }
 
     return 0;
 }
