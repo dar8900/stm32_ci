7228921e0f6ff4c2e8f9eb6fa4b228269bdb8308
Dario Sadel VB <dar8900@gmail.com>
2023-05-30 15:34:20 +0200

    Aggiunti moduli per la gestione di un semplice timer

---
+++ hmt_hal/common/hmt_simple_timer.c
@@ -0,0 +1,50 @@
+#include "hmt_simple_timer.h"
+
+void hmt_SimpleTimerStart(simple_timer *Timer, uint32_t Timeout)
+{
+	if(!Timer){
+		return;
+	}
+	if(Timeout != 0 && Timeout != Timer->timeout){
+		Timer->timeout = Timeout;
+	}
+	Timer->start = true;
+	Timer->stop = false;
+	Timer->counter = hmt_GetMsTick();
+}
+
+void hmt_SimpleTimerStop(simple_timer *Timer)
+{
+	if(!Timer){
+		return;
+	}
+	Timer->start = false;
+	Timer->stop = true;	
+}
+
+void hmt_SimpleTimerRestart(simple_timer *Timer, uint32_t Timeout)
+{
+	hmt_SimpleTimerStop(Timer);
+	hmt_SimpleTimerStart(Timer, Timeout);
+}
+
+
+bool hmt_SimpleTimerElapsed(simple_timer *Timer, bool Restart, uint32_t Timeout)
+{
+	bool Elapsed = false;
+	if(!Timer){
+		return false;
+	}
+	if(!Timer->start){
+		return false;
+	}
+	if(hmt_GetMsTick() - Timer->counter >= Timer->timeout)
+	{
+		hmt_SimpleTimerStop(Timer);
+		if(Restart){
+			hmt_SimpleTimerRestart(Timer, Timeout);
+		}
+		Elapsed = true;
+	}
+	return Elapsed;
+}
---
+++ hmt_hal/common/hmt_simple_timer.h
@@ -0,0 +1,41 @@
+#ifndef HMT_SIMPLE_TIMER_H
+#define HMT_SIMPLE_TIMER_H
+
+#include "hmt_common.h"
+
+/**
+ * @brief Start the timer with a timeout
+ * 
+ * @param simple_timer* Timer 
+ * @param uint32_t Timeout 
+ */
+void hmt_SimpleTimerStart(simple_timer *Timer, uint32_t Timeout);
+
+/**
+ * @brief Stop the timer
+ * 
+ * @param simple_timer* Timer 
+ */
+void hmt_SimpleTimerStop(simple_timer *Timer);
+
+/**
+ * @brief Restart the timer with optionally a new timeout if != 0 
+ * 			or != from the old one
+ * 
+ * @param simple_timer* Timer
+ * @param uint32_t Timeout 
+ */
+void hmt_SimpleTimerRestart(simple_timer *Timer, uint32_t Timeout);
+
+/**
+ * @brief Check if the timer is elapsed, if Restart = true, the timer restarting 
+ * 			with optionally a new timeout if != 0 or != from the old one
+ * 
+ * @param simple_timer* Timer
+ * @param bool Restart 
+ * @param uint32_t Timeout 
+ * @return [bool] Elsapsed 
+ */
+bool hmt_SimpleTimerElapsed(simple_timer *Timer, bool Restart, uint32_t Timeout);
+
+#endif
