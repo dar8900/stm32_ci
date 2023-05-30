adb7686f446441adbc7da04a246056f36eadc522
Dario Sadel VB <dar8900@gmail.com>
2023-05-30 17:05:43 +0200

    Aggiunti moduli iniziali per la gestione della usart

---
+++ hmt_hal/usart/usart_manager.c
@@ -0,0 +1,8 @@
+#include "usart_manager.h"
+#include "rcc_manager.h"
+
+
+void hmt_UsarInit()
+{
+	
+}
---
+++ hmt_hal/usart/usart_manager.h
@@ -0,0 +1,7 @@
+#ifndef HMT_USART_MANAGER_H
+#define HMT_USART_MANAGER_H
+#include "hmt_common.h"
+
+
+
+#endif
