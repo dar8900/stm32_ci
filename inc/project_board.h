739fe1ad2e00cdb74de93ef3fe1988c4318e4982
Dario Sadel VB <dar8900@gmail.com>
2023-06-07 09:39:00 +0200

    Aggiunto commento per i valori da impostare per i gpio

--- inc/project_board.h
+++ inc/project_board.h
@@ -12,10 +12,13 @@
 #endif
 
 /*
-#define    P<port>_<pin>_MODE    	NOT_USED
-#define    P<port>_<pin>_SPEED    	SPEED_HIGH
-#define    P<port>_<pin>_OUTPUT    	PUSH_PULL
-#define    P<port>_<pin>_PULL_TYPE   NO_PULL
+#define    P<port>_<pin>_MODE    		NOT_USED
+#define    P<port>_<pin>_SPEED    		SPEED_HIGH
+#define    P<port>_<pin>_OUTPUT    		PUSH_PULL
+#define    P<port>_<pin>_PULL_TYPE  	NO_PULL
+#define    P<port>_<pin>_AF_TYPE   		NO_PULL
+#define    P<port>_<pin>_ACTIVE_LEVEL   NO_PULL
+#define    P<port>_<pin>_START_LEVEL   	NO_PULL
 */
 
 
