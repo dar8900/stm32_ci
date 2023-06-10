8eba7652001c0019d61555f2885b376c38adf5bb
jarvis-kde <dar8900@gmail.com>
2023-06-10 18:08:00 +0200

    Riaggiunge cartelle src e inc e modificato gitignore

---
+++ inc/project_board.h
@@ -0,0 +1,40 @@
+#ifndef PROJECT_BOARD
+#define PROJECT_BOARD
+
+#include "hmt_common.h"
+
+// #define USE_HSI
+#define USE_HSE
+
+// #define USE_USART
+#ifdef USE_USART
+	#define USART_USED			USART1
+#endif
+
+// #define USE_SPI
+#ifdef USE_SPI
+	#define SPI_USED			SPI1
+#endif
+
+/*
+#define    P<port>_<pin>_MODE    		NOT_USED
+#define    P<port>_<pin>_SPEED    		SPEED_HIGH
+#define    P<port>_<pin>_OUTPUT    		PUSH_PULL
+#define    P<port>_<pin>_PULL_TYPE  	NO_PULL
+#define    P<port>_<pin>_AF_TYPE   		NO_PULL
+#define    P<port>_<pin>_ACTIVE_LEVEL   NO_PULL
+#define    P<port>_<pin>_START_LEVEL   	NO_PULL
+*/
+
+
+#define PB_3_MODE		OUTPUT
+#define PB_3_SPEED		SPEED_HIGH
+#define PB_3_OUTPUT		PUSH_PULL
+#define PB_3_PULL_TYPE	NO_PULL
+
+#define PC_13_MODE		OUTPUT
+#define PC_13_SPEED		SPEED_HIGH
+#define PC_13_OUTPUT	PUSH_PULL
+#define PC_13_PULL_TYPE	NO_PULL
+
+#endif
