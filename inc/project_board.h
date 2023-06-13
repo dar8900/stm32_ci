6bfc39ce0cf8e32494ede175adf13eba319d9cfb
Dario Sadel VB <dar8900@gmail.com>
2023-06-13 12:49:25 +0200

    Modificato main e platform per avere un branch master pulito dalle varie periferice, i test singoli verrano implementati nei vari branch relativi

--- inc/project_board.h
+++ inc/project_board.h
@@ -6,7 +6,7 @@
 // #define USE_HSI
 #define USE_HSE
 
-#define USE_USART
+// #define USE_USART
 #ifdef USE_USART
 	#define USART_USED			USART1
 #endif
@@ -26,24 +26,6 @@
 #define    P<port>_<pin>_START_LEVEL   	LOW
 */
 
-/* USART 1 */
-/* TX */
-#define PA_9_MODE		ALTERNATE
-#define PA_9_SPEED		SPEED_HIGH
-#define PA_9_OUTPUT		PUSH_PULL
-#define PA_9_PULL_TYPE	NO_PULL
-
-/* RX */
-#define PA_10_MODE		INPUT
-#define PA_10_PUL_TYPE	PULL_UP
-#define PA_10_SPEED		SPEED_HIGH
-
-
-
-#define PB_3_MODE		OUTPUT
-#define PB_3_SPEED		SPEED_HIGH
-#define PB_3_OUTPUT		PUSH_PULL
-#define PB_3_PULL_TYPE	NO_PULL
 
 #define PC_13_MODE		OUTPUT
 #define PC_13_SPEED		SPEED_HIGH
--- src/main.c
+++ src/main.c
@@ -6,22 +6,12 @@ int main(void)
     simple_timer_t ToggleTimer;
     hmt_RccInit();
     hmt_GpioInit();
-#ifdef USE_USART    
-    hmt_UsartInit(USART_USED, 115200);
-#endif
     hmt_SimpleTimerStart(&ToggleTimer, 50);
-    uint32_t NewTimeout = 50;
     for (;;)
     {
-        if(hmt_SimpleTimerElapsed(&ToggleTimer, true, NewTimeout))
+        if(hmt_SimpleTimerElapsed(&ToggleTimer, true, 0))
         {
             hmt_GpioTogglePin(PC_13_ID);
-            hmt_GpioTogglePin(PB_3_ID);
-            if(NewTimeout < 500)
-                NewTimeout += 2;
-            else
-                NewTimeout = 50;
-            hmt_UsartSendMsg(USART_USED, "Hello world!", strlen("Hello world!"));
         }
     }
 
