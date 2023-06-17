1948b74639cee324fd953bd914279789cd6061ce
Dario ArchMatebook <dar8900@gmail.com>
2023-06-17 21:11:17 +0200

    Aggiunta scrittura di test su spi

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "spi_common.h"
 
 char *TestMSg = "Hello from stm32f103 with SPI\n";
 
@@ -13,7 +14,7 @@ int main(void)
         if(hmt_SimpleTimerElapsed(&ToggleTimer, true, 0))
         {
             hmt_GpioTogglePin(DEBUG_LED);
-            
+            hmt_SPIMasterTransmit(SPI1, TestMSg, strlen(TestMSg), 0);
         }
     }
 
