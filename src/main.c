13eed710cee4c79b322f57d5ced33eec874797ce
Dario Sadel VB <dar8900@gmail.com>
2023-06-23 09:11:23 +0200

    Aggiornato main per test adc

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "adc_manager.h"
+#include "adc_common.h"
 
 
 int main(void)
@@ -6,6 +8,7 @@ int main(void)
     simple_timer_t ToggleTimer;
     hmt_RccInit();
     hmt_GpioInit();
+    hmt_Adc_Init(ADC_USED, DMA_USED);
     hmt_SimpleTimerStart(&ToggleTimer, 500);
     for (;;)
     {
@@ -13,6 +16,7 @@ int main(void)
         {
             hmt_GpioTogglePin(DEBUG_LED);
         }
+        hmt_AdcDataHandle();
     }
 
     return 0;
