71a255938475bd011d62d4b3815b9a2a80e75d70
Dario Sadel VB <dar8900@gmail.com>
2023-06-26 13:14:27 +0200

    Modificato main per test spi

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "spi_manager.h"
+#include "spi_common.h"
 
 
 int main(void)
@@ -6,12 +8,14 @@ int main(void)
     simple_timer_t ToggleTimer;
     hmt_RccInit();
     hmt_GpioInit();
+    hmt_Sp(SPI_USED);
     hmt_SimpleTimerStart(&ToggleTimer, 500);
     for (;;)
     {
         if(hmt_SimpleTimerElapsed(&ToggleTimer, true, 0))
         {
             hmt_GpioTogglePin(DEBUG_LED);
+            hmt_SPIMasterTransmit(SPI_USED, "Hi from stm32f103\r\n", strlen("Hi from stm32f103\r\n"), 0);
         }
     }
 
