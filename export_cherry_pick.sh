2953f332b87cb6bb869e4684397c86091eca2822
Dario Sadel VB <dar8900@gmail.com>
2023-06-28 10:20:43 +0200

    Aggiunto scritp per esportare il cherry pick di una commit in ogni branch Aggiornato script per aggiornare i submodule

---
+++ export_cherry_pick.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ottieni l'elenco dei branch locali
+branches=$(git branch --format='%(refname:short)')
+
+# Specifica la commit da cherry-pickare
+commit=$1
+
+if [ -z "$commit" ];then
+	echo "Inserire la commit sha"
+	exit 1
+fi	
+
+# Itera su ogni branch
+for branch in $branches; do
+  # Passa al branch corrente
+  git checkout $branch
+  
+  # Esegui il cherry-pick della commit specificata
+  git cherry-pick $commit
+done
+
