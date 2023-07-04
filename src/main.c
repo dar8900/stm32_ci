37b61fcf5f3a41fabb9fc67548a486db5df76b46
Dario Sadel VB <dar8900@gmail.com>
2023-07-04 10:08:30 +0200

    Modificato main per test exti

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
+static void GpioItHandler()
+{
+    hmt_GpioTogglePin(DEBUG_LED);
+}
+
 int main(void)
 {
     simple_timer_t ToggleTimer;
     hmt_RccInit();
     hmt_GpioInit();
+    hmt_GpioSetEXTI_IT_Handler(IT_PIN, GpioItHandler);
     hmt_SimpleTimerStart(&ToggleTimer, 50);
     for (;;)
     {
-        if(hmt_SimpleTimerElapsed(&ToggleTimer, true, 0))
-        {
-            hmt_GpioTogglePin(DEBUG_LED);
-            
-        }
+
     }
 
     return 0;
