de138ab93e3c22fd542b7ecccb9b608ec4067e34
Dario Sadel VB <dar8900@gmail.com>
2023-08-31 15:37:58 +0200

    Scritto test per it gpio

--- src/interrupt_gpio_test.c
+++ src/interrupt_gpio_test.c
@@ -1,11 +1,19 @@
 #include "interrupt_gpio_test.h"
+#include "main.h"
+
+static void GpioITHandler()
+{
+	hmt_GpioTogglePin(DEBUG_LED);
+}
 
 void InterruptGpioTestInit()
 {
-
+	hmt_GpioSetEXTI_IT_Handler(PA_8_ID, GpioITHandler);
 }
+
 
 void InterruptGpioTestRun()
 {
 	
+
 }
