bd1854f950ed329dffb716c9a48405241c1d21fe
Dario Sadel VB <dar8900@gmail.com>
2023-09-06 16:06:35 +0200

    Aggiornato test per interrupt gpio

--- src/interrupt_gpio_test.c
+++ src/interrupt_gpio_test.c
@@ -5,11 +5,13 @@
 static void GpioITHandler()
 {
 	hmt_GpioTogglePin(LED_TEST);
+	hmt_UsartSendMsg(USART_USED, (uint8_t *)"EXTI Attivo!\r\n", strlen("EXTI Attivo!\r\n"));
 }
 
 void InterruptGpioTestInit()
 {
-	hmt_GpioSetEXTI_IT_Handler(PB_12_ID, GpioITHandler);
+	hmt_GpioSetEXTI_IT_Handler(EXTI_PIN, GpioITHandler);
+	hmt_GpioSetEXTI_IT_Handler(USER_BUTTON, GpioITHandler);
 }
 
 
